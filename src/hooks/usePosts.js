e5329a5120f7ade9dd556f59c11704672f57034f
MinhCreator <92376650+MinhCreator@users.noreply.github.com>
2026-06-25 02:45:46 +0700

    fix usePost.js not pagination

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchPosts } from "../services/api";
 
-export function usePosts(page, limit = 4) {
+export function usePosts(page, limit = 3) {
   return useQuery({
     queryKey: ["posts", page],
     queryFn: () => fetchPosts(page, limit),
-    staleTime: 5 * 60 * 1000,
+    staleTime: 0,
   });
 }
